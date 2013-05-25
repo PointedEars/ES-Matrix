388f682734374bb40aac1477e57dbf1271d3a054
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2013-05-25 18:55:26 +0000

    Read database connection configuration from file

--- application/models/databases/es-matrix/MatrixDb.php
+++ application/models/databases/es-matrix/MatrixDb.php
@@ -4,11 +4,5 @@ require_once 'lib/Db/MySQLDB.php';
 
 class MatrixDb extends MySQLDB
 {
-  protected $_host     = 'localhost';
-  protected $_dbname   = 'es-matrix';
-  protected $_username = 'root';
-  protected $_password = 'r2d2c3rt';
-  
-  /* NOTE: Value must be written exactly so, see MySQL manual */
-  protected $_charset  = 'utf8';
+  protected $_dbname = 'es-matrix';
 }
