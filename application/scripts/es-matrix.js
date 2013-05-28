99e9c3fcf7268b5bcd387772ae4c0dd01dc12f6e
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2013-05-28 13:35:10 +0000

    * application/scripts/es-matrix.js: Fixed feature table filter for Edit mode

--- application/scripts/es-matrix.js
+++ application/scripts/es-matrix.js
@@ -6,6 +6,7 @@ function body_load ()
   var elements = new Array();
   elements[0] = document.getElementById('abstract');
   elements[1] = document.getElementById('foreword');
+  elements[2] = document.getElementById('features');
   if (elements[0])
   {
     jsx.dom.hyphenate(elements);
@@ -25,9 +26,23 @@ function body_load ()
     properties);
 
   var filter = document.forms[0].elements["filter"];
-  es_matrix.timeout = new jsx.dom.timeout.Timeout(function () {
-    table.applyFilter(filter.value);
-  });
+  if (!filter)
+  {
+    /*
+     * In Edit mode, there is no form for the filter input,
+     * and the first column contains Edit/Delete commands.
+     */
+    filter = document.getElementById("filter");
+    filterColumns[0].index = 1;
+  }
+
+  /* No Timeout without filter input */
+  if (filter)
+  {
+    es_matrix.timeout = new jsx.dom.timeout.Timeout(function () {
+      table.applyFilter(filter.value);
+    });
+  }
 }
 
 function table_click (e)
@@ -45,18 +60,92 @@ function table_click (e)
 
   if (target.tagName.toLowerCase() == "th"
       && target.parentNode.cells[1] == target
-      && target.firstChild.tagName.toLowerCase() != "textarea")
+      && target.firstChild.nodeName.toLowerCase() != "form")
   {
-    target.innerHTML =
-        '<form action="index-db.php" method="POST">'
-      + '<textarea name="code" style="width: 100%; height: 3em">'
-      + unsynhl(target.innerHTML)
-      + '</textarea>'
-      + '<input type="hidden" name="controller" value="feature">'
-      + '<input type="hidden" name="action" value="save">'
-      + '<input type="hidden" name="id" value="' + target.id.match(/\d+/)[0] + '">'
-      + '<button type="submit" name="metadataOnly" value="1" style="clear: both; float: right">'
-      + 'Save</button>'
-      + '</form>';
+    var oldHTML = target.innerHTML;
+    jsx.dom.removeChildren(target, target.childNodes);
+    target.appendChild(jsx.dom.createNodeFromObj({
+      type: "form",
+      properties: {
+        action: "index-db",
+        method: "POST",
+        onsubmit: function (ev) {
+          var req = new jsx.net.http.Request();
+          req.getDataFromForm(this, true);
+          req.setData(req.data + "&xhr=1");
+          req.setSuccessListener(function (response) {
+            target.innerHTML = synhl(response.responseText);
+          });
+
+          if (req.send())
+          {
+            console.log(req);
+            return false;
+          }
+        }
+      },
+      childNodes: [
+        {
+          type: "textarea",
+          properties: {
+            name: "code",
+            style: {
+              width: "100%",
+              height: "3em"
+            }
+          },
+          childNodes: [unsynhl(oldHTML)]
+        },
+        {
+          type: "input",
+          properties: {
+            type: "hidden",
+            name: "controller",
+            value: "feature"
+          }
+        },
+        {
+          type: "input",
+          properties: {
+            type: "hidden",
+            name: "action",
+            value: "save"
+          }
+        },
+        {
+          type: "input",
+          properties: {
+            type: "hidden",
+            name: "id",
+            value: target.id.match(/\d+/)[0]
+          }
+        },
+        {
+          type: "button",
+          properties: {
+            type: "submit",
+            name: "metadataOnly",
+            value: "1",
+            style: {
+              clear: "both",
+              "float": "right"
+            }
+          },
+          childNodes: ["Save"]
+        }
+      ]
+    }));
+
+//    .innerHTML =
+//        '<form action="index-db.php" method="POST">'
+//      + '<textarea name="code" style="width: 100%; height: 3em">'
+//      + unsynhl(target.innerHTML)
+//      + '</textarea>'
+//      + '<input type="hidden" name="controller" value="feature">'
+//      + '<input type="hidden" name="action" value="save">'
+//      + '<input type="hidden" name="id" value="' + target.id.match(/\d+/)[0] + '">'
+//      + '<button type="submit" name="metadataOnly" value="1" style="clear: both; float: right">'
+//      + 'Save</button>'
+//      + '</form>';
   }
 }
